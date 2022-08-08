0b08763ba953fb935d2faeaa75beffce9488cbaf
hieunh020201 <89857083+hieunh020201@users.noreply.github.com>
2022-08-08 17:04:21 +0700

    Initial commit

---
+++ src/main/java/com/hieunh/SmartPhonesWeb/SmartPhonesWebApplication.java
@@ -0,0 +1,13 @@
+package com.hieunh.SmartPhonesWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPhonesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPhonesWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hieunh/SmartPhonesWeb/SmartPhonesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hieunh.SmartPhonesWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPhonesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
